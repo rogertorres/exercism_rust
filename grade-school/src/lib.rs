bff5348fe20627c69ddd5f89e8729a3cfb06e12b
rogertorres <rogertps@gmail.com>
2021-01-28 22:31:04 -0300

    finished grade-school

--- grade-school/src/lib.rs
+++ grade-school/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 // This annotation prevents Clippy from warning us that `School` has a
 // `fn new()` with no arguments, but doesn't implement the `Default` trait.
 //
@@ -5,19 +7,26 @@
 // case, we want to keep things relatively simple. The `Default` trait is not the point
 // of this exercise.
 #[allow(clippy::new_without_default)]
-pub struct School {}
+pub struct School<'a> {
+    grades: BTreeMap<&'a str, u32>,
+}
 
-impl School {
-    pub fn new() -> School {
-        unimplemented!()
+impl<'a> School<'a> {
+    pub fn new() -> School<'a> {
+        School{
+            grades: BTreeMap::new(),
+        }
     }
 
-    pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!("Add {} to the roster for {}", student, grade)
-    }
+    pub fn add(&mut self, grade: u32, student: &'a str) {
+        self.grades.insert(student,grade);
 
-    pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+    }
+    // The exercise expects a Vec, but I think HashSet would be better. 
+    pub fn grades(&self) -> Vec<u32> { 
+        let mut v = self.grades.values().cloned().collect::<Vec<u32>>();
+        v.dedup();
+        v
     }
 
     // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
@@ -25,6 +34,10 @@ impl School {
     // the internal structure can be completely arbitrary. The tradeoff is that some data
     // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!("Return the list of students in {}", grade)
+        self.grades
+            .iter()
+            .filter(|(_, g)| *g == &grade)
+            .map(|(s,_)| s.to_string())
+            .collect()
     }
 }
